038e917fc3c9972eb11b19f435281212bb08084c
Shtepselevich <sinelnikov.romann@yandex.ru>
2023-12-09 12:51:56 +0300

    feat: created js for burger

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -1,5 +1,19 @@
+// Бургер
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("burger").addEventListener("click", function () {
     document.querySelector(".header").classList.toggle("open");
+    if (document.querySelector(".header").classList.contains("open")) {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
   });
+  document
+    .querySelectorAll(".navbar-menu__item, .header-menu__link")
+    .forEach((item) => {
+      item.addEventListener("click", function (event) {
+        document.querySelector(".header").classList.remove("open");
+        document.body.classList.remove("no-scroll");
+      });
+    });
 });
