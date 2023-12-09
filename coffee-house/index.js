2c55e37587ed77ed41a8d821bda50e96e2c05059
Shtepselevich <sinelnikov.romann@yandex.ru>
2023-12-09 20:01:26 +0300

    feat: createt logic for swtich slide by click butt

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -17,3 +17,35 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
 });
+// Слайдер
+document.addEventListener("DOMContentLoaded", function () {
+  const sliderLine = document.querySelector(".slider-line");
+  const sliderBtnNext = document.querySelector(".slider-button__next");
+  const sliderBtnPrev = document.querySelector(".slider-button__prev");
+  const totalSlides = document.querySelectorAll(".slider-content").length;
+  let currentSlide = 0;
+
+  sliderBtnNext.addEventListener("click", function () {
+    if (currentSlide < totalSlides - 1) {
+      // Если не последний слайд, сдвигаем на 100%
+      currentSlide++;
+      sliderLine.style.marginLeft = `-${currentSlide * 100}%`;
+    } else {
+      // Если последний слайд, возвращаемся на начало
+      currentSlide = 0;
+      sliderLine.style.marginLeft = "0";
+    }
+  });
+
+  sliderBtnPrev.addEventListener("click", function () {
+    if (currentSlide > 0) {
+      // Если не первый слайд, сдвигаем на 100% назад
+      currentSlide--;
+      sliderLine.style.marginLeft = `-${currentSlide * 100}%`;
+    } else {
+      // Если первый слайд, переходим на последний
+      currentSlide = totalSlides - 1;
+      sliderLine.style.marginLeft = `-${currentSlide * 100}%`;
+    }
+  });
+});
