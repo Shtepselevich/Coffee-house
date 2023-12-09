a77fd2b2efa184ad08f9624db4ac949900137ff2
Shtepselevich <sinelnikov.romann@yandex.ru>
2023-12-09 21:41:07 +0300

    feat: created progress bar

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -22,30 +22,103 @@ document.addEventListener("DOMContentLoaded", function () {
   const sliderLine = document.querySelector(".slider-line");
   const sliderBtnNext = document.querySelector(".slider-button__next");
   const sliderBtnPrev = document.querySelector(".slider-button__prev");
+  const sliderPaginationButtons = document.querySelectorAll(
+    ".slider-pagination__button"
+  );
+  const progressBarWrappers = document.querySelectorAll(".slider-progress");
   const totalSlides = document.querySelectorAll(".slider-content").length;
   let currentSlide = 0;
+  let intervalId;
 
-  sliderBtnNext.addEventListener("click", function () {
+  const startAutoPlay = () => {
+    intervalId = setInterval(() => {
+      moveToNextSlide();
+    }, 5000);
+  };
+
+  const stopAutoPlay = () => {
+    clearInterval(intervalId);
+  };
+
+  const moveToNextSlide = () => {
     if (currentSlide < totalSlides - 1) {
-      // Если не последний слайд, сдвигаем на 100%
       currentSlide++;
-      sliderLine.style.marginLeft = `-${currentSlide * 100}%`;
     } else {
-      // Если последний слайд, возвращаемся на начало
       currentSlide = 0;
-      sliderLine.style.marginLeft = "0";
     }
+    updateSlider();
+    updatePagination();
+    updateProgressBar();
+  };
+
+  const moveToPrevSlide = () => {
+    if (currentSlide > 0) {
+      currentSlide--;
+    } else {
+      currentSlide = totalSlides - 1;
+    }
+    updateSlider();
+    updatePagination();
+    updateProgressBar();
+  };
+
+  const updateSlider = () => {
+    sliderLine.style.marginLeft = `-${currentSlide * 100}%`;
+  };
+
+  const updatePagination = () => {
+    sliderPaginationButtons.forEach((button, index) => {
+      button.classList.toggle("active", index === currentSlide);
+    });
+  };
+
+  const updateProgressBar = () => {
+    const progressBar = progressBarWrappers[currentSlide];
+    progressBar.style.width = "0%";
+
+    const slideDuration = 5000; // Длительность показа слайда (в миллисекундах)
+    const progressBarDuration = 5000; // Длительность анимации прогресс-бара (в миллисекундах)
+
+    let progress = 0;
+    const updateProgress = () => {
+      progress += (100 / progressBarDuration) * 1000;
+      progressBar.style.width = `${progress}%`;
+
+      if (progress < 100) {
+        setTimeout(updateProgress, 1000);
+      }
+    };
+
+    updateProgress();
+
+    setTimeout(() => {
+      progress = 0;
+      progressBar.style.width = "0%";
+    }, slideDuration);
+  };
+
+  sliderBtnNext.addEventListener("click", () => {
+    moveToNextSlide();
+    stopAutoPlay();
+    startAutoPlay();
   });
 
-  sliderBtnPrev.addEventListener("click", function () {
-    if (currentSlide > 0) {
-      // Если не первый слайд, сдвигаем на 100% назад
-      currentSlide--;
-      sliderLine.style.marginLeft = `-${currentSlide * 100}%`;
-    } else {
-      // Если первый слайд, переходим на последний
-      currentSlide = totalSlides - 1;
-      sliderLine.style.marginLeft = `-${currentSlide * 100}%`;
-    }
+  sliderBtnPrev.addEventListener("click", () => {
+    moveToPrevSlide();
+    stopAutoPlay();
+    startAutoPlay();
   });
+
+  sliderPaginationButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      currentSlide = index;
+      updateSlider();
+      updatePagination();
+      updateProgressBar();
+      stopAutoPlay();
+      startAutoPlay();
+    });
+  });
+
+  startAutoPlay();
 });
